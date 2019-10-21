ab1afb95ead4074791fe48b2aba2a6c71f94f175
KateDoan <kate.tradoan@gmail.com>
2019-10-21 14:55:54 +0800

    Initial Commit

---
+++ R/microcancertemp-package.R
@@ -0,0 +1,15 @@
+#' microcancertemp: A temporary package for micro-simulation for cancers
+#'
+#'@docType package
+#'@name microcancertemp
+NULL
+
+## usethis namespace: start
+#' @useDynLib microcancertemp, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
